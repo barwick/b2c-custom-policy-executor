6cf9c9fa4885a9e2fd60ae1be7da910b6bc55b48
Tom Barwick <tom_barwick@hotmail.com>
2023-03-29 10:40:25 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+let bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// Test echo endpoint
+app.get("/hello", (req, res) => {
+  res.status(200).send("World!");
+});
+
+// Validation code for access codes - used by B2C custom policy
+app.post("/validate-accesscode", (req, res) => {
+  let accessCode = "88888";
+  if (accessCode == req.body.accessCode) {
+    res.status(200).send();
+  } else {
+    let errorResponse = {
+      version: "1.0",
+      status: 409,
+      code: "errorCode",
+      requestId: "requestId",
+      userMessage:
+        "The access code you entered is incorrect. Please try again.",
+      developerMessage: `The The provided code ${req.body.accessCode} does not match the expected code for user.`,
+      moreInfo:
+        "https://docs.microsoft.com/en-us/azure/active-directory-b2c/string-transformations",
+    };
+    res.status(409).send(errorResponse);
+  }
+});
+
+app.listen(80, () => {
+  console.log(`Access code service listening on port !` + 80);
+});
